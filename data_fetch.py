81b3c620840565b43f8ef05b5196538eaedc7a8c
ThatGuyOnGithubRn <140696155+ThatGuyOnGithubRn@users.noreply.github.com>
2025-06-01 01:08:51 +0530

    Update data_fetch.py

--- data_fetch.py
+++ data_fetch.py
@@ -1,5 +1,7 @@
 import requests
 from datetime import datetime, timezone
+import pandas as pd
+
 
 def contract_launch_time(symbol):
     response = requests.get(f"https://api.india.delta.exchange/v2/products/{symbol}")
@@ -9,10 +11,10 @@ def contract_launch_time(symbol):
     launch_time = datetime.fromisoformat(meta["launch_time"].replace("Z", "+00:00"))
     return launch_time
 
-symbol = "C-BTC-102000-130625"
 
 
-def fetch_week_data(i):
+all_data = {}
+def fetch_week_data(i,symbol):
     launch_time = contract_launch_time(symbol)
 
     start_unix = int(launch_time.timestamp())+i*604800
@@ -32,7 +34,39 @@ def fetch_week_data(i):
     else:
         candles = response.json().get("result", [])
         print(f"Retrieved {len(candles)} candles for {symbol}")
-        for candle in candles:
-            print(candle)
+        all_data[symbol] = candles
 
-fetch_week_data(0)
+
+
+url = "https://api.india.delta.exchange/v2/products"
+headers = {"Accept": "application/json"}
+
+resp = requests.get(url, headers=headers)
+resp.raise_for_status()
+products = resp.json()["result"]
+
+
+for p in products:
+    if (
+        (p.get("contract_type") == "call_options" or p.get("contract_type") == "put_options") and
+        p.get("settlement_time", "").startswith("2025-06")
+    ):
+        symbol = p.get("symbol")
+        strike = p.get("strike_price")
+        expiry = p.get("settlement_time")
+        # print(f"sybmol: {symbol} etrike: {strike} expiry: {expiry}")
+        fetch_week_data(0,symbol)
+
+
+import os
+
+os.makedirs("symbol_data", exist_ok=True)
+
+for symbol, candles in all_data.items():
+    df = pd.DataFrame(candles, columns=["timestamp", "open", "high", "low", "close", "volume"])
+    df["timestamp"] = pd.to_datetime(df["timestamp"], unit="s")
+    df["symbol"] = symbol
+
+    file_name = f"symbol_data/{symbol}.csv"
+    df.to_csv(file_name, index=False)
+    print(f"Saved {symbol} to {file_name}")
